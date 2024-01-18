02bcada01877c5933576bff461c40a44945c5ace
Sergey Arc <sergey-vd@outlook.com>
2024-01-18 07:50:24 +0300

    урок 3

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'welcome';
+    return view('welcome');
 });
 
 Route::get('/about', function () {
-    return view('about');
+    $team = [
+        ['name' => 'Hodor', 'position' => 'programmer'],
+        ['name' => 'Joker', 'position' => 'CEO'],
+        ['name' => 'Elvis', 'position' => 'CTO'],
+    ];
+
+    return view('about', ['team' => $team]);
+});
+
+Route::get('/articles', function () {
+    return view('articles');
 });
