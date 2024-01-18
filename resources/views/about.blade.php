d51b6c9647062bbda41599e8bc4303a6bdfc7080
Sergey Arc <sergey-vd@outlook.com>
2024-01-18 09:15:59 +0300

    урок 4

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,22 +1,8 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel | About</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}"></script>
-    </head>
-    <body>
-        <div class="container mt-4">
-            <!-- BEGIN (write your solution here) -->
-            <h1>О блоге</h1>
-            <p>Эксперименты с Ларавелем на Хекслете</p>
+@extends('layouts.app')
 
-            @foreach ($team as $member)
-                <p>{{ $member['name'] }}</p>
-            @endforeach
-            <!-- END -->
-        </div>
-    </body>
-</html>
+@section('title', 'О блоге')
+
+@section('content')
+    <h1>О блоге</h1>
+    <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
